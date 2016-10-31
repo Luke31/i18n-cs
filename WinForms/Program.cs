c097208c8c9329a534a34386d2d3056556f22d3c
Luke31 <lukas.m.schmid@gmail.com>
2016-10-31 15:26:11 +0900

    Add project files.

---
+++ WinForms/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+// C#
+using System.Globalization;
+namespace WinForms
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //Uncomment to force specific locale
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("ja-JP"); //Japanese (Default of this project)
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("en-GB"); //English
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
