983a70ac07350b25989c3fcaf44453cc1dbdf897
Luke31 <lukas.m.schmid@gmail.com>
2016-11-01 17:25:02 +0900

    WPF application added, Tutorial completed

---
+++ WPF/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var txtblk = new TextBlock()
+            {
+                Text = "Hello Code",
+                VerticalAlignment = VerticalAlignment.Bottom,
+                HorizontalAlignment = HorizontalAlignment.Left
+            };
+            
+            var image = new Image
+            {
+                Source = new BitmapImage(new Uri("Images/en.png", UriKind.RelativeOrAbsolute)),
+                VerticalAlignment = VerticalAlignment.Bottom,
+                HorizontalAlignment = HorizontalAlignment.Right,
+                Stretch = Stretch.None
+            };
+
+            contentGrid.Children.Add(image);
+            contentGrid.Children.Add(txtblk);
+        }
+    }
+}
