e1d021c004f0fec32d483f6bb69e540957a7dc32
Luke31 <lukas.m.schmid@gmail.com>
2016-11-11 12:02:02 +0900

    Fixed WPFLocalizationExtensin-Project to same layout as new simple project

--- WPFLocalizationExtension/Loc/LocResources.Designer.cs
+++ WPFLocalizationExtension/Loc/LocResources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace WPFLocalizationExtension {
+namespace WPFLocalizationExtension.Loc {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace WPFLocalizationExtension {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WPFLocalizationExtension.LocResources", typeof(LocResources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WPFLocalizationExtension.Loc.LocResources", typeof(LocResources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- WPFLocalizationExtension/MainWindow.xaml.cs
+++ WPFLocalizationExtension/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WPFLocalizationExtension.Extensions;
+using WPFLocalizationExtension.Loc;
 using WPFLocalizeExtension.Engine;
 
 namespace WPFLocalizationExtension
