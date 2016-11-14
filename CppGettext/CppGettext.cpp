af24d90c518c3b5baba47d2f4b1f29a0ebdb8d4a
Luke31 <lukas.m.schmid@gmail.com>
2016-11-14 16:13:30 +0900

    default cpp application

--- CppGettext/CppGettext.cpp
+++ CppGettext/CppGettext.cpp
@@ -2,10 +2,49 @@
 //
 
 #include "stdafx.h"
+#include <string>
+#include <iostream>
+#include <io.h> //for setting outputmode UNICODE
+#include <fcntl.h> //contains _O_U16TEXT
 
 
-int main()
+int PrintUserLocale() {
+	LCID lcid = GetUserDefaultLCID();
+	WCHAR strNameBuffer[LOCALE_NAME_MAX_LENGTH];
+	DWORD error = ERROR_SUCCESS;
+	//Evaluate locale
+	if (LCIDToLocaleName(lcid, strNameBuffer, LOCALE_NAME_MAX_LENGTH, 0) == 0)
+	{
+		error = GetLastError();
+		return 1;
+	}
+	else
+	{
+		wprintf(L"Locale: %s\n", strNameBuffer);
+		return 0;
+	}
+}
+
+//wmain: http://stackoverflow.com/a/3299860/2003325
+int wmain(int argc, wchar_t* argv[])
 {
-    return 0;
+	_setmode(_fileno(stdout), _O_U16TEXT); //_O_WTEXT (with BOM)
+										   //stdout may now be written to file (First character must be ASCII if output is written to file)
+	std::wcout << L"Enabling Unicode support" << std::endl;
+
+	PrintUserLocale();
+
+	//Load multi-lang resource
+	//std::wstring str = LoadStringW(IDS_HW);
+	std::wstring str = L"test";
+
+	//Output using wprintf
+	wprintf(str.c_str());
+	std::wcout << std::endl;
+
+	//Output using wcout
+	std::wcout << str << std::endl;
+
+	return 0;
 }
 
